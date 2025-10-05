7107a3657124c6c90fd33ec130c83726b8225781
나의찬 <cab07036@naver.com>
2025-10-06 05:17:08 +0900

    Create TripgetherBeApplicationTests.java

---
+++ src/test/java/com/tripgether/be/TripgetherBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tripgether.be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripgetherBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

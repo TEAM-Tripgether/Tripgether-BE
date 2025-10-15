772e8ac4e58db3f2596dac8d37aee13f2a327a26
SUH SAECHAN <chan4760@gmail.com>
2025-10-15 17:44:02 +0900

    단일 프로젝트 구조다중패키지 멀티프로젝트 구조로 변경 필요 : feat :멀티프로세스 변경 https://github.com/TEAM-Tripgether/Tripgether-BE/issues/16

---
+++ TG-Web/src/main/java/com/tripgether/web/config/ComponentScanConfig.java
@@ -0,0 +1,18 @@
+package com.tripgether.web.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 컴포넌트 스캔 설정
+ * 모든 모듈의 패키지를 스캔하여 Bean으로 등록
+ */
+@Configuration
+@ComponentScan(basePackages = {
+    "com.tripgether.global",      // TG-Common의 global 패키지
+    "com.tripgether.example",     // TG-Example 도메인
+    "com.tripgether.web"          // TG-Web 레이어
+})
+public class ComponentScanConfig {
+}
+

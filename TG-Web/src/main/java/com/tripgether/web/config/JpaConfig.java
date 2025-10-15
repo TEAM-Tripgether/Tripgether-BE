b377071af53a3ec9ebdb46faf235a03b46c452fc
SUH SAECHAN <83532821+Cassiiopeia@users.noreply.github.com>
2025-10-16 01:17:11 +0900

    Example 모듈 Member 모듈로 교체 필요 : refactor : 포매팅 처리 https://github.com/TEAM-Tripgether/Tripgether-BE/issues/19

--- TG-Web/src/main/java/com/tripgether/web/config/JpaConfig.java
+++ TG-Web/src/main/java/com/tripgether/web/config/JpaConfig.java
@@ -3,12 +3,7 @@ package com.tripgether.web.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-/**
- * JPA 설정
- * BaseEntity의 @CreatedDate, @LastModifiedDate를 위한 JPA Auditing 활성화
- */
+/** JPA 설정 BaseEntity의 @CreatedDate, @LastModifiedDate를 위한 JPA Auditing 활성화 */
 @Configuration
 @EnableJpaAuditing
-public class JpaConfig {
-}
-
+public class JpaConfig {}

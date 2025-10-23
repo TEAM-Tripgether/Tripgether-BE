5d0975c0bb9d2976f7b29a8103e4044c3ba29554
SUH SAECHAN <83532821+Cassiiopeia@users.noreply.github.com>
2025-10-23 19:03:50 +1100

    로그인 API 호출시 Failed to fetch 오류 발생 : refactor : Swagger 내부 server 추가 코드 개선 https://github.com/TEAM-Tripgether/Tripgether-BE/issues/31

--- TG-Web/src/main/java/com/tripgether/web/config/SwaggerConfig.java
+++ TG-Web/src/main/java/com/tripgether/web/config/SwaggerConfig.java
@@ -1,49 +1,62 @@
 package com.tripgether.web.config;
 
+import com.tripgether.common.properties.SpringDocProperties;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.customizers.OpenApiCustomizer;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
 @OpenAPIDefinition(
     info = @io.swagger.v3.oas.annotations.info.Info(
         title = "📚 Tripgether : 여행의 동반자 📚",
         description = """
             ### http://suh-project.synology.me:8093
             """
-    ),
-    servers = {
-        @Server(url = "https://api.tripgether.suhsaechan.kr", description = "메인 서버"),
-        @Server(url = "http://localhost:8080", description = "로컬 서버")
-    }
+    )
 )
+@Configuration
+@RequiredArgsConstructor
+@EnableConfigurationProperties(SpringDocProperties.class)
 public class SwaggerConfig {
+
+  private final SpringDocProperties springDocProperties;
 
   @Bean
   public OpenAPI tripgetherOpenAPI() {
-    SecurityScheme bearerAuth =
-        new SecurityScheme().type(SecurityScheme.Type.HTTP)
-            .scheme("bearer")
-            .bearerFormat("JWT")
-            .in(SecurityScheme.In.HEADER)
-            .name("Authorization");
+    SecurityScheme bearerAuth = new SecurityScheme()
+        .type(SecurityScheme.Type.HTTP)
+        .scheme("bearer")
+        .bearerFormat("JWT")
+        .in(SecurityScheme.In.HEADER)
+        .name("Authorization");
 
     return new OpenAPI()
         .components(new Components().addSecuritySchemes("Bearer Token", bearerAuth))
-        .addSecurityItem(new SecurityRequirement().addList("Bearer Token"))
-        .servers(List.of(
-            new io.swagger.v3.oas.models.servers.Server()
-                .url("https://api.tripgether.suhsaechan.kr")
-                .description("메인 서버"),
-            new io.swagger.v3.oas.models.servers.Server()
-                .url("http://localhost:8080")
-                .description("로컬 서버")
-        ));
+        .addSecurityItem(new SecurityRequirement().addList("Bearer Token"));
+//        .servers(List.of(
+//            new io.swagger.v3.oas.models.servers.Server()
+//                .url("https://api.tripgether.suhsaechan.kr")
+//                .description("메인 서버"),
+//            new io.swagger.v3.oas.models.servers.Server()
+//                .url("http://localhost:8080")
+//                .description("로컬 서버")
+//        ));
+  }
+
+  @Bean
+  public OpenApiCustomizer serverCustomizer() {
+    return openApi -> {
+      springDocProperties.getServers().forEach(server ->
+          openApi.addServersItem(new io.swagger.v3.oas.models.servers.Server()
+              .url(server.getUrl())
+              .description(server.getDescription()))
+      );
+    };
   }
 }

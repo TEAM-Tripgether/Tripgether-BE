cd7329993e9ada86d486e41577fa33602b936875
SUH SAECHAN <83532821+Cassiiopeia@users.noreply.github.com>
2025-11-08 00:13:24 +0900

    기본 인스타 릴스 이미지 동영상 데이터 추출 API 구현 : fix : UpdateServiceAgreementTermsResponse 로 명명 수정 https://github.com/TEAM-Tripgether/Tripgether-BE/issues/64

--- TG-Member/src/main/java/com/tripgether/member/dto/updateServiceAgreementTermsResponse.java
+++ TG-Member/src/main/java/com/tripgether/member/dto/updateServiceAgreementTermsResponse.java
@@ -1,6 +1,8 @@
 package com.tripgether.member.dto;
 
 import com.tripgether.member.constant.OnboardingStep;
+import com.tripgether.member.entity.Member;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 @Getter
@@ -8,7 +10,13 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class updateServiceAgreementTermsResponse {
+public class UpdateServiceAgreementTermsResponse {
+  @Schema(description = "현재 온보딩 단계", example = "NAME")
   private OnboardingStep currentStep;
+
+  @Schema(description = "온보딩 상태", example = "IN_PROGRESS")
   private String onboardingStatus;
+
+  @Schema(description = "회원 정보 (디버깅용)")
+  private MemberDto member;
 }

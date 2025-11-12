e2153c1a2a72967abe22fec94ff7716bc4fb685a
강지윤 <23011595@sju.ac.kr>
2025-11-12 15:51:10 +0900

    사용자 정보수정 API 요청 : fix :  롬북 어노테이션 사용 https://github.com/TEAM-Tripgether/Tripgether-BE/issues/61

--- TG-Member/src/main/java/com/tripgether/member/dto/InterestDto.java
+++ TG-Member/src/main/java/com/tripgether/member/dto/InterestDto.java
@@ -1,35 +1,14 @@
 package com.tripgether.member.dto;
 
 import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
-/*@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor*/
+@Setter
+@Getter
+@AllArgsConstructor
 public class InterestDto {
   private UUID id;
   private String name;
-
-  // 기본 생성자
-  public InterestDto(UUID id, String name) {
-    this.id = id;
-    this.name = name;
-  }
-
-  // Getter & Setter
-  public UUID getId() {
-    return id;
-  }
-
-  public void setId(UUID id) {
-    this.id = id;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
 }

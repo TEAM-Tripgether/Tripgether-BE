49950695a9e68e34e97eead0a0aa8f62debc2ed3
강지윤 <23011595@sju.ac.kr>
2025-11-20 18:49:44 +0900

    메인 화면 최근 SNS 콘텐츠 목록 조회 : fix : 관심사 3개 이상 선택 수정 https://github.com/TEAM-Tripgether/Tripgether-BE/issues/78

--- TG-Member/src/main/java/com/tripgether/member/dto/onboarding/request/UpdateInterestsRequest.java
+++ TG-Member/src/main/java/com/tripgether/member/dto/onboarding/request/UpdateInterestsRequest.java
@@ -2,7 +2,7 @@ package com.tripgether.member.dto.onboarding.request;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -22,7 +22,7 @@ public class UpdateInterestsRequest {
   @JsonIgnore
   private UUID memberId;
 
-  @NotEmpty(message = "관심사는 최소 1개 이상 선택해야 합니다.")
+  @Size(min = 3, message = "관심사는 최소 3개 이상 선택해야 합니다.")
   @Schema(description = "관심사 ID 목록", example = "[\"550e8400-e29b-41d4-a716-446655440000\"]", required = true)
   private List<UUID> interestIds;
 }
